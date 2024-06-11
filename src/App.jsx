d410dc66cef32bcb37ef65c0430c8353c32a285e
Veroclementine <veroclementine2000@gmail.com>
2024-06-11 17:46:24 +0200

    projet terminé. Ajout du dossier helpers et du fichier index.js pour les fonctions de calcul du simulateur.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Button from "./components/Button";
-import { formatArgent } from "./helpers";
+import { formatArgent, calculTotalPayer } from "./helpers";
 
 function App() {
   const [montant, setMontat] = useState(10000);
   const [mois, setMois] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [paiement, setPaiement] = useState(0);
+
+  useEffect(() => {
+    const resultTotalPayer = calculTotalPayer(montant, mois);
+    setTotal(resultTotalPayer);
+  }, [montant, mois]);
+
+  //Calcul de la mensualité
+  useEffect(() => {
+    setPaiement(total / mois);
+  }, [total]);
 
   //estas variables no van a cambiar por eso no se usa useState
   const MIN = 0;
@@ -42,6 +54,7 @@ function App() {
     <div className="my-20 max-w-lg mx-auto bg-white rounded-lg shadow p-10 ">
       <Header />
 
+      {/* buttons - +  */}
       <div className="flex justify-between my-6">
         <Button operator="-" fn={handleClickDiminution} />
         <Button
@@ -60,10 +73,12 @@ function App() {
         value={montant}
       />
 
+      {/* chifre pret */}
       <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
         {formatArgent(montant)}
       </p>
 
+      {/* choisir delai de paiment */}
       <h2 className="text-2xl font-extrabold text-gray-500 text-center">
         Choisir un <span className="text-indigo-600">délai </span> de paiement
       </h2>
@@ -76,7 +91,28 @@ function App() {
         <option value="12">12 Mois</option>
         <option value="24">24 Mois</option>
       </select>
-      
+
+      <div className="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          Récapitulatif <span className="text-indigo-600">des paiements </span>
+        </h2>
+
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {mois} Mois
+        </p>
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {" "}
+          {formatArgent(paiement)} Mensualité
+        </p>
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {formatArgent(total)} Total à payer
+        </p>
+      </div>
+
+      <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+              Développe par: <span className='text-indigo-600'>Veronica Paredes</span>
+          </h2>
+          <a className='block mb-5 text-center' target="_blank" rel="noreferrer noopener" href="https://veronicaparedes.net">Visitez mon site web.</a>
     </div>
   );
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,11 +1,43 @@
-const formatArgent = (value)=>{
-    const formatter = new Intl.NumberFormat('fr-FR', {
-        style: 'currency',
-        currency: 'EUR'
-    });
-    return formatter.format(value);
-}
+//Function pour format argent
 
-export {
-    formatArgent
-}
+const formatArgent = (value) => {
+  const formatter = new Intl.NumberFormat("fr-FR", {
+    style: "currency",
+    currency: "EUR",
+  });
+  return formatter.format(value);
+};
+
+//function pour calculer le montant total à payer
+
+const calculTotalPayer = (montant, delai) => {
+  let total;
+
+  //plus le montant demandé est élevé, plus l'intérêt est faible
+
+  if (montant < 5000) {
+    total = montant * 1.5;
+  } else if (montant >= 5000 && montant < 10000) {
+    total = montant * 1.4;
+  } else if (montant >= 10000 && montant < 15000) {
+    total = montant * 1.3;
+  } else {
+    total = montant * 1.2;
+  }
+
+  //Delai - Plus la durée est longue, plus les intérêts sont élevés
+
+  if (delai === 6) {
+    total *= 1.1;
+  } else if (delai === 12) {
+    total *= 1.2;
+  } else {
+    total *= 1.3;
+  }
+  return total;
+};
+
+export { 
+    formatArgent,
+    calculTotalPayer,
+ };
